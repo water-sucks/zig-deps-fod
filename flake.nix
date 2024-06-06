1de299e045aa19ed43a62ddd15513ccd40b17fae
Varun Narravula <varun@snare.dev>
2024-06-06 15:06:22 -0700

    feat: first commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Fixed-output derivation for fetching Zig dependencies";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = nixpkgs.lib.systems.flakeExposed;
+
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          name = "zig-deps-fod-shell";
+          packages = with pkgs; [zig];
+        };
+      };
+    };
+}
