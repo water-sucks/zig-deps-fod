51e1e337ad3243a42e1cd8a19701ae2fc45148f6
Varun Narravula <varun@snare.dev>
2024-06-17 16:05:10 -0700

    feat: add example project

---
+++ example/flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "An example project that uses Zig";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    flake-compat.url = "github:edolstra/flake-compat";
+
+    zig-deps-fod.url = "../.";
+  };
+
+  outputs = {
+    nixpkgs,
+    zig-deps-fod,
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = nixpkgs.lib.systems.flakeExposed;
+
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          name = "example";
+          packages = [pkgs.zig];
+        };
+
+        packages.default = let
+          inherit (pkgs) stdenv zig;
+          inherit (zig-deps-fod.lib) fetchZigDeps;
+
+          deps = fetchZigDeps {
+            name = "example";
+            inherit stdenv zig;
+            src = ./.;
+            depsHash = "sha256-BZ7usVI2tcdcrZkbnQAVTD9ntX/8gnrWXwvbPcgX+6o=";
+          };
+        in
+          stdenv.mkDerivation {
+            pname = "example";
+            version = "0.0.1";
+            src = ./.;
+
+            postPatch = ''
+              mkdir -p .cache
+              ln -s ${deps} .cache/p
+
+              ls .cache/p
+            '';
+
+            nativeBuildInputs = [zig];
+
+            dontConfigure = true;
+            dontInstall = true;
+
+            buildPhase = ''
+              mkdir -p $out
+              zig build install \
+                --cache-dir $(pwd)/zig-cache \
+                --global-cache-dir $(pwd)/.cache \
+                -Dcpu=baseline \
+                -Doptimize=ReleaseSafe \
+                --prefix $out
+            '';
+          };
+      };
+    };
+}
---
+++ example/src/main.zig
@@ -0,0 +1,38 @@
+const clap = @import("clap");
+const std = @import("std");
+
+const debug = std.debug;
+const io = std.io;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const params = comptime clap.parseParamsComptime(
+        \\-h, --help             Display this help and exit.
+        \\-n, --number <usize>   An option parameter, which takes a value.
+        \\-s, --string <str>...  An option parameter which can be specified multiple times.
+        \\<str>...
+        \\
+    );
+
+    var diag = clap.Diagnostic{};
+    var res = clap.parse(clap.Help, &params, clap.parsers.default, .{
+        .diagnostic = &diag,
+        .allocator = gpa.allocator(),
+    }) catch |err| {
+        // Report useful error and exit
+        diag.report(io.getStdErr().writer(), err) catch {};
+        return err;
+    };
+    defer res.deinit();
+
+    if (res.args.help != 0)
+        return clap.usage(std.io.getStdErr().writer(), clap.Help, &params);
+    if (res.args.number) |n|
+        debug.print("--number = {}\n", .{n});
+    for (res.args.string) |s|
+        debug.print("--string = {s}\n", .{s});
+    for (res.positionals) |pos|
+        debug.print("{s}\n", .{pos});
+}
