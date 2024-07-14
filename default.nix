f0a57cdf06f2100d7a045a4062592aeb89468868
Varun Narravula <varun@snare.dev>
2024-07-14 16:30:42 -0700

    fix: fetch lazy dependencies (#2)
    * fix: fetch dependencies recursively
    
    * fix: sed lazy fetching out of build.zig.zon

--- default.nix
+++ default.nix
@@ -21,13 +21,41 @@ stdenv.mkDerivation {
 
   buildPhase = ''
     runHook preBuild
-    zig build --fetch --global-cache-dir ".zig-cache" --cache-dir ".zig-cache"
+
+    root_dir=$PWD
+    zig_cache="$PWD/.zig-cache"
+
+    package_dirs_checked=()
+
+    function fetchDepsForPackage() {
+      package="$1"
+
+      if [[ "''${package_dirs_checked[*]}" =~ $package ]]; then
+        return 0
+      fi
+      package_dirs_checked+=("$package")
+
+      if [ -e "$package/build.zig.zon" ]; then
+        sed -i '/^\s*\.lazy\s*=\s*true\s*,\s*$/d' "$package/build.zig.zon"
+
+        cd "$package" || exit 1
+        zig build --fetch --global-cache-dir "$zig_cache" --cache-dir "$zig_cache"
+        for dir in "$zig_cache"/p/*; do
+          fetchDepsForPackage "$dir"
+        done
+      fi
+    }
+
+    fetchDepsForPackage .
+
+    cd $root_dir
+
     runHook postBuild
   '';
 
   installPhase = ''
     runHook preInstall
-    cp -r --reflink=auto .zig-cache/p $out
+    cp -r --reflink=auto "${packageRoot}/.zig-cache/p" $out
     runHook postInstall
   '';
 
